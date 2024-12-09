bb143bc947e7d841903b41ebd47f046a554e66a8
LUIS GERARDO AGUILAR GARCIA <100291529+LuisAguilarDev@users.noreply.github.com>
2024-12-09 18:41:57 -0500

    feat: day3 parser approach

--- src/day3/day3.ts
+++ src/day3/day3.ts
@@ -36,3 +36,29 @@ export function resultOfEnabledMultiplication(rawData: string) {
   });
   return total;
 }
+//Parser approach
+//TODO: Build as AST
+function lexer(input: string): string[] {
+  const tokens = [];
+  const re = /mul\((\d{1,3}),(\d{1,3})\)/g;
+  let match;
+  while ((match = re.exec(input)) !== null) {
+    tokens.push(match[1] + ',' + match[2]);
+  }
+
+  return tokens;
+}
+
+function operate(tokens: string[]) {
+  let sum = 0;
+  for (const operation of tokens) {
+    const numbers = operation.split(',').map(Number);
+    sum += numbers[0] * numbers[1];
+    console.log(sum);
+  }
+  return sum;
+}
+const tokens = lexer(
+  'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))',
+);
+operate(tokens);
