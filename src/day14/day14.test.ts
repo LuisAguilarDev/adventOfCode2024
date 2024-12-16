7debe3017d8f4f28d0a04fcc0eed3f5839c7ad6d
LUIS GERARDO AGUILAR GARCIA <100291529+LuisAguilarDev@users.noreply.github.com>
2024-12-15 23:03:33 -0500

    feat: day 14 solution

---
+++ src/day14/day14.test.ts
@@ -0,0 +1,40 @@
+import fs from 'fs';
+import { getRobots, getSafetyFactor, Robot, timeToShowPicture } from './day14';
+
+describe('Day 14', () => {
+  let data: Robot[];
+  beforeAll(async () => {
+    data = await fs.promises
+      .readFile('./src/day14/day14.txt')
+      .then((bufferContent) => getRobots(bufferContent.toString()));
+  });
+  test('#1 it should give the fewest tokens you would have to spend to win all possible prizes', () => {
+    expect(data).toBeDefined();
+  });
+  test('#2 it should give the fewest tokens you would have to spend to win all possible prizes', () => {
+    expect(
+      getSafetyFactor(
+        11,
+        7,
+        getRobots(`p=0,4 v=3,-3
+                   p=6,3 v=-1,-3
+                   p=10,3 v=-1,2
+                   p=2,0 v=2,-1
+                   p=0,0 v=1,3
+                   p=3,0 v=-2,-2
+                   p=7,6 v=-1,-3
+                   p=3,0 v=-1,-2
+                   p=9,3 v=2,3
+                   p=7,3 v=-1,2
+                   p=2,4 v=2,-3
+                   p=9,5 v=-3,-3`),
+      ),
+    ).toBe(12);
+  });
+  test('#2 it should give the fewest tokens you would have to spend to win all possible prizes', () => {
+    expect(getSafetyFactor(103, 101, data)).toBe(211692000);
+  });
+  //   test('#2 it should give the fewest tokens you would have to spend to win all possible prizes', () => {
+  //     expect(timeToShowPicture(103, 101, data)).toBe(211692000);
+  //   });
+});
---
+++ src/day14/day14.ts
@@ -0,0 +1,136 @@
+//--- Day 14: Restroom Redoubt ---
+
+//Part1
+export class Robot {
+  startPosition: { c: number; r: number };
+  velocity: { c: number; r: number };
+  constructor(
+    startPosition: { c: number; r: number },
+    velocity: { c: number; r: number },
+  ) {
+    this.startPosition = startPosition;
+    this.velocity = velocity;
+  }
+}
+export class Point {
+  c: number;
+  r: number;
+  constructor(r: number, c: number) {
+    this.r = r;
+    this.c = c;
+  }
+}
+export function getRobots(rawData: string): Robot[] {
+  const robots: Robot[] = [];
+  rawData.split('\n').forEach((row) => {
+    const regex = /-?\d+/g;
+    const [sc, sr, vc, vr] = row.match(regex)?.map(Number) ?? [0, 0, 0, 0];
+    robots.push(new Robot({ c: sc, r: sr }, { c: vc, r: vr }));
+  });
+  return robots;
+}
+export function getSafetyFactor(
+  IROWS: number,
+  ICOLS: number,
+  robots: Robot[],
+): number {
+  const time = 100;
+  const middleR = Math.floor(IROWS / 2);
+  const middleC = Math.floor(ICOLS / 2);
+  let [q1, q2, q3, q4] = [0, 0, 0, 0];
+  for (const robot of robots) {
+    console.log('🚀 ~ robot:', robot);
+    const r = robot.startPosition.r;
+    const c = robot.startPosition.c;
+    const dr = robot.velocity.r;
+    const dc = robot.velocity.c;
+    let nr = (((r + dr * time) % IROWS) + IROWS) % IROWS;
+    let nc = (((c + dc * time) % ICOLS) + ICOLS) % ICOLS;
+    if (nr < middleR && nc < middleC) {
+      q1++;
+    }
+    if (nr < middleR && nc > middleC) {
+      q2++;
+    }
+    if (nr > middleR && nc < middleC) {
+      q3++;
+    }
+    if (nr > middleR && nc > middleC) {
+      q4++;
+    }
+  }
+  return q1 * q2 * q3 * q4;
+}
+
+//Part2
+export function timeToShowPicture(
+  IROWS: number,
+  ICOLS: number,
+  robots: Robot[],
+): number {
+  let time = 1;
+  // prettier-ignore
+  const directions = [[-1, -1],[-1, 0],[-1, 1],[0, -1],[0, 1],[1, -1],[1, 0],[1, 1]];
+  let maxClusterSize = 0;
+  while (true) {
+    const robotsPosition: Point[] = [];
+    const visited = new Set<string>();
+    for (const robot of robots) {
+      const r = robot.startPosition.r;
+      const c = robot.startPosition.c;
+      const dr = robot.velocity.r;
+      const dc = robot.velocity.c;
+      let nr = (((r + dr * time) % IROWS) + IROWS) % IROWS;
+      let nc = (((c + dc * time) % ICOLS) + ICOLS) % ICOLS;
+      robotsPosition.push(new Point(nr, nc));
+    }
+    robotsPosition.sort((a, b) => a.c - b.c || a.r - b.r);
+    const positionsSet = new Set<string>(
+      robotsPosition.map((p) => `${p.r},${p.c}`),
+    );
+    for (const { r, c } of robotsPosition) {
+      const key = `${r},${c}`;
+      if (!visited.has(key)) {
+        const clusterSize = bfs(r, c, positionsSet, visited);
+        if (clusterSize > maxClusterSize) {
+          maxClusterSize = clusterSize;
+          console.log('🚀 ~ maxClusterSize:', maxClusterSize, time);
+          //wait a big cluster
+          //   console.log
+          //   🚀 ~ maxClusterSize: 124 6587
+
+          //     at timeToShowPicture (src/day14/day14.ts:98:19)
+
+          // console.log
+          //   🚀 ~ maxClusterSize: 229 6587
+        }
+      }
+    }
+    time++;
+  }
+
+  function bfs(
+    startR: number,
+    startC: number,
+    positionsSet: Set<string>,
+    visited: Set<string>,
+  ) {
+    visited.add(`${startR},${startC}`);
+    const queue = [[startR, startC]];
+    let clusterCount = 0;
+    while (queue.length > 0) {
+      const [r, c] = queue.shift()!;
+      clusterCount++;
+      for (const [dr, dc] of directions) {
+        const nr = r + dr;
+        const nc = c + dc;
+        const key = `${nr},${nc}`;
+        if (positionsSet.has(key) && !visited.has(key)) {
+          visited.add(key);
+          queue.push([nr, nc]);
+        }
+      }
+    }
+    return clusterCount;
+  }
+}
