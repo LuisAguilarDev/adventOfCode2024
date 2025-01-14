10d5d7781d9eeabf6bf74d5090561fc6d3a17ea9
LUIS GERARDO AGUILAR GARCIA <100291529+LuisAguilarDev@users.noreply.github.com>
2025-01-13 22:42:59 -0500

    feat: day22 solution

---
+++ src/day22/day22.test.ts
@@ -0,0 +1,60 @@
+import fs from 'fs';
+import { bestSeq, getData, getNextSecret, sumOfSecrets } from './day22';
+
+describe('Day 22', () => {
+  let data: number[];
+  let sampleData: number[];
+  beforeAll(async () => {
+    data = await fs.promises
+      .readFile('./src/day22/day22.txt')
+      .then((bufferContent) => getData(bufferContent.toString()));
+    sampleData = getData(`1
+                          10
+                          100
+                          2024`);
+  });
+
+  test('#1 should define data and sampleData properly', () => {
+    expect(data).toBeDefined();
+    expect(sampleData).toBeDefined();
+  });
+
+  test('#2 it should return the next ten secrets numbers correctly', () => {
+    let result = getNextSecret(123n);
+    expect(result).toBe(15887950n);
+    result = getNextSecret(result);
+    expect(result).toBe(16495136n);
+    result = getNextSecret(result);
+    expect(result).toBe(527345n);
+    result = getNextSecret(result);
+    expect(result).toBe(704524n);
+    result = getNextSecret(result);
+    expect(result).toBe(1553684n);
+    result = getNextSecret(result);
+    expect(result).toBe(12683156n);
+    result = getNextSecret(result);
+    expect(result).toBe(11100544n);
+    result = getNextSecret(result);
+    expect(result).toBe(12249484n);
+    result = getNextSecret(result);
+    expect(result).toBe(7753432n);
+    result = getNextSecret(result);
+    expect(result).toBe(5908254n);
+  });
+  test('#3 it should return the next ten secrets numbers correctly', () => {
+    let result = sumOfSecrets(sampleData, 2000);
+    expect(result).toBe(37327623);
+  });
+  test('#4 it should return the next ten secrets numbers correctly', () => {
+    let result = sumOfSecrets(data, 2000);
+    expect(result).toBe(13022553808);
+  });
+  test('#5 it should return the next ten secrets numbers correctly', () => {
+    let result = bestSeq([1, 2, 3, 2024]);
+    expect(result).toBe(23);
+  });
+  test('#6 it should return the next ten secrets numbers correctly', () => {
+    let result = bestSeq(data);
+    expect(result).toBe(1555);
+  });
+});
---
+++ src/day22/day22.ts
@@ -0,0 +1,70 @@
+//--- Day 22: Monkey Market ---
+
+import { sumOfComplexities } from '../day21/day21';
+
+export function getData(rawString: string): number[] {
+  return rawString.split('\n').map((str) => Number(str.trim()));
+}
+// Part 1
+
+export function getNextSecret(num: bigint): bigint {
+  num = ((num * 64n) ^ num) % 16777216n;
+  num = ((num / 32n) ^ num) % 16777216n;
+  num = ((num * 2048n) ^ num) % 16777216n;
+  return num;
+}
+
+export function sumOfSecrets(secrets: number[], iterations: number): number {
+  let result = 0;
+  for (const secret of secrets) {
+    result += getLastSecret(secret, iterations);
+  }
+  return result;
+}
+export function getLastSecret(secret: number, iterations: number): number {
+  let nextSecret: bigint = BigInt(secret);
+  for (let i = 0; i < iterations; i++) {
+    nextSecret = getNextSecret(nextSecret);
+  }
+  return Number(nextSecret);
+}
+
+// Part 2
+export function getHigherPrice(
+  secret: number,
+  sumAllSeqs: Map<string, number>,
+): void {
+  let seqs: number[] = [];
+  let prevSecret: bigint = BigInt(secret);
+  const visited = new Set();
+  for (let i = 0; i < 2000; i++) {
+    let nextSecret = getNextSecret(prevSecret);
+    let prevPrice = Number(prevSecret % 10n);
+    let nextPrice = Number(nextSecret % 10n);
+    let diff = nextPrice - prevPrice;
+    seqs.push(diff);
+    prevSecret = nextSecret;
+    if (seqs.length < 4) continue;
+    if (seqs.length === 5) {
+      seqs.shift();
+    }
+    const hash = seqs.toString();
+    if (visited.has(hash)) continue;
+    visited.add(hash);
+    let price = sumAllSeqs.get(hash);
+    if (!price) sumAllSeqs.set(hash, nextPrice);
+    else {
+      sumAllSeqs.set(hash, price + nextPrice);
+    }
+  }
+}
+
+export function bestSeq(secrets: number[]): number {
+  const sumAllSeqs: Map<string, number> = new Map();
+  for (const secret of secrets) {
+    getHigherPrice(secret, sumAllSeqs);
+  }
+  const max = Math.max(0, ...sumAllSeqs.values());
+  return max;
+}
+// bestSeq([1, 2, 3, 2024]);
